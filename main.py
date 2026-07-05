b0debfcb0030f0cf09ec163eb205fdb12c6d5a3e
Abuzar Haneefi <abuzarhaneefi@icloud.com>
2026-07-05 15:16:07 +0530

    it still does not work

--- main.py
+++ main.py
@@ -2,18 +2,23 @@ import typer
 
 app = typer.Typer()
 
+lists = []
+
 
 @app.command()
 def hello(name: str):
     print(f"Hello {name}")
 
-def create_list(name: str):
-   name = []
-   return name
 
 @app.command()
-def add_item(item: str, list_name:str)
-    if list
+def create_list(name: str):
+    lists.append(name)
+    print(f"list {name} created")
+
+
+# @app.command()
+# def add_item(item: str, list_name:str)
+#     if list
 
 
 if __name__ == "__main__":
